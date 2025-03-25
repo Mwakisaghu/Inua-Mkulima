7410e7792880de93307a2152097b54ef83563966
unknown <harrisonchambela2016@gmail.com>
2025-03-25 13:02:20 +0300

    This adds transacton handler to handle farmer transactions

--- src/main/handlers/ApiHandler.java
+++ src/main/handlers/ApiHandler.java
@@ -2,16 +2,16 @@ package com.example;
 
 import com.example.handlers.AuthHandler;
 import com.example.handlers.ProductHandler;
+import com.example.handlers.TransactionHandler;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ApiHandler implements HttpHandler {
     private static final AuthHandler authHandler = new AuthHandler();
     private static final ProductHandler productHandler = new ProductHandler();
+    private static final TransactionHandler transactionHandler = new TransactionHandler();
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws IOException {
@@ -22,6 +22,8 @@ public class ApiHandler implements HttpHandler {
             authHandler.handleRequest(exchange);
         } else if ("/api/products".equals(path)) {
             productHandler.handleRequest(exchange);
+        } else if ("/api/transactions".equals(path) && "POST".equals(method)) {
+            transactionHandler.handleRequest(exchange);
         } else {
             exchange.setStatusCode(404);
             exchange.getResponseSender().send("Not Found");
